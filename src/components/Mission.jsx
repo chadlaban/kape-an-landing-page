51c44ea285df8ff7528e4391192419f22493d1ab
chadlaban <laban.chad@gmail.com>
2023-09-05 16:00:42 +0800

    Added useState and handleState for form (error when entering values in input element.)

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Mission = () => {
+    const [formData, setFormData] = useState({
+        fullName: "",
+        email: "",
+        feedback: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
     return (
         <div className='text-white max-w-[1240px] mx-auto grid md:grid-cols-2 py-14'>
             <div className='text-center'>
@@ -10,24 +21,24 @@ const Mission = () => {
                 </article>
             </div>
             <div className='max-w-[1240px] mx-20 mt-4'>
-                <form onSubmit='' className="bg-[#D7D8D7] rounded-xl space-y-6 p-6 text-xl">
+                <form onSubmit='' className="bg-[#D7D8D7] text-[#000300] rounded-xl space-y-6 p-6 text-xl">
                     <div className=''>
                         <label htmlFor="fullName" className="block text-[#000300]">
                             Full Name:
                         </label>
-                        <input type="text" id="fullName" name="fullName" value='' onChange='' className="mt-1 p-2 border rounded-md w-full" required />
+                        <input type="text" id="fullName" name="fullName" value={formData.fullName} onChange={handleChange} className="mt-1 p-2 border rounded-md w-full" required />
                     </div>
                     <div>
                         <label htmlFor="email" className="block text-[#000300]">
                             Email:
                         </label>
-                        <input type="email" id="email" name="email" value='' onChange='' className="mt-1 p-2 border rounded-md w-full" required />
+                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 p-2 border rounded-md w-full" required />
                     </div>
                     <div>
                         <label htmlFor="feedback" className="block text-[#000300]">
                             Feedback:
                         </label>
-                        <textarea id="feedback" name="feedback" value='' onChange='' className="mt-1 p-2 border rounded-md w-full" rows="4" required></textarea>
+                        <textarea id="feedback" name="feedback" value={formData.feedback} onChange={handleChange} className="mt-1 p-2 border rounded-md w-full" rows="4" required></textarea>
                     </div>
                     <div className='pb-10'>
                         <button type="submit" className="bg-[#4D4E49] text-white font-light px-6 py-2 rounded-md hover:bg-[#4D4E49] scale-105 float-right">
